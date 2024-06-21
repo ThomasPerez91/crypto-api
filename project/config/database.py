56ef1c99939138562378e47684f231067ec32248
ThomasPerez91 <dialldialldiall@gmail.com>
2024-06-21 22:31:56 +0200

    feat: add alembic

--- project/config/database.py
+++ project/config/database.py
@@ -5,20 +5,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base  # type: ignore
 
 load_dotenv()
 
-# Charger les variables d'environnement
-POSTGRES_DB: str = os.getenv("POSTGRES_DB")
-POSTGRES_USER: str = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_HOST: str = os.getenv("POSTGRES_HOST")
-POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")
-
 # Utiliser une chaîne multi-lignes pour éviter les problèmes de formatage avec Prettier
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{POSTGRES_USER}:"
-    f"{POSTGRES_PASSWORD}@"
-    f"{POSTGRES_HOST}:{POSTGRES_PORT}/"
-    f"{POSTGRES_DB}"
-)
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 # Créer l'engine SQLAlchemy
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
